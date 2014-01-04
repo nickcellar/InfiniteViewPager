c6eec482499ab641a344fd9c9c012f04393b0401
Nicholas Wong <nickwph@gmail.com>
2014-01-04 21:24:42 +0800

    Changed background in testing activity.

--- test/src/com/nicholasworkshop/test/MainActivity.java
+++ test/src/com/nicholasworkshop/test/MainActivity.java
@@ -9,20 +9,30 @@ import com.nicholasworkshop.ui.InfiniteViewPager;
 
 public class MainActivity extends Activity
 {
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		InfiniteViewPager mInfinitePageView = (InfiniteViewPager) findViewById(R.id.InfinitePageView);
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        InfiniteViewPager mInfinitePageView = (InfiniteViewPager) findViewById(R.id.InfinitePageView);
 
-		for (int i = 0; i < 3; i++) {
-			TextView textView = new TextView(this);
-			textView.setText("Bonjour Page " + i);
-			textView.setBackgroundColor(Color.BLUE);
-			textView.setTextColor(Color.WHITE);
-			textView.setTextSize(30);
-			mInfinitePageView.addPage(textView);
-		}
-	}
+        for (int i = 0; i < 3; i++) {
+            TextView textView = new TextView(this);
+            textView.setText("Page " + i);
+            textView.setTextColor(Color.WHITE);
+            textView.setTextSize(30);
+            switch (i % 3) {
+                case 0:
+                    textView.setBackgroundColor(Color.RED);
+                    break;
+                case 1:
+                    textView.setBackgroundColor(Color.BLUE);
+                    break;
+                case 2:
+                    textView.setBackgroundColor(Color.GREEN);
+                    break;
+            }
+            mInfinitePageView.addPage(textView);
+        }
+    }
 }
